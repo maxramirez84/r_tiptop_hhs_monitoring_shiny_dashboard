20373fc66b5cfbeaa90c6b25a2ed21facca9f0ba
maxramirez84 <maxramirez84@gmail.com>
2019-05-14 17:08:59 +0200

    First headers and output text
    Adapt maxramirez84/r_tiptop_baseline_hhs_data_quality_report to Shiny
    
    Include general and first section headers. Include the message indicating how many records do we have stored currently in REDCap and the timestamp of the last record transferred.

--- hhs_progress_quality_dashboard/app.R
+++ hhs_progress_quality_dashboard/app.R
@@ -1,13 +1,29 @@
 library(shiny)
 
+source("../tiptop_hhs_quality.R")
+source("../tokens.R")
+
 # Define UI ----
 ui <- fluidPage(
-   
+  h1("TIPTOP Baseline HHS Data Quality Report: MADAGASCAR"),
+      
+  h2("FIELD DATA COLLECTION PROGRESS"),
+  helpText(textOutput("records_summary"))
 )
 
 # Define server logic ----
 server <- function(input, output) {
-   
+  api_url   <- redcap_api_url
+  api_token <- api_token_test # Testing project
+  
+  hhs_data <- readData(api_url, api_token)
+  
+  number_of_records = numberOfRecords(hhs_data)
+  last_record_date = lastRecordDate(hhs_data)
+  output$records_summary <- renderText({
+    paste0("The database contains ", number_of_records, " records ", 
+           "(last record from ", last_record_date, ").")
+  })
 }
 
 # Run the application 
